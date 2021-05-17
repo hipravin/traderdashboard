2b8806fc6f409070c53d45f1c3c934032ff5af4f
Alex <alexander.vmk@gmail.com>
2021-05-17 16:20:24 +0300

    initial commit - empty spring boot app

---
+++ src/main/java/com/hipravin/traderdashboard/TraderdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.hipravin.traderdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderdashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hipravin/traderdashboard/TraderdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hipravin.traderdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
